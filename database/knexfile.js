957edda4ce60eb9a8a40bea9c697771c48339cd0
Phan Van Tai <vantai2004kt@gmail.com>
2026-04-07 21:11:40 +0700

    feat(DB): update knexjs

--- database/knexfile.js
+++ database/knexfile.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 module.exports = {
   development: {
     client: "mysql2",
@@ -9,6 +11,21 @@ module.exports = {
       database: process.env.DB_NAME || "graduation_db",
 
       // Needed because legacy SQL files contain multiple statements.
+      multipleStatements: true,
+    },
+    migrations: {
+      directory: "./knex_migrations",
+      tableName: "knex_migrations",
+    },
+  },
+  railway: {
+    client: "mysql2",
+    connection: {
+      host: process.env.DB_HOST || "gondola.proxy.rlwy.net",
+      port: Number(process.env.DB_PORT || 44327),
+      user: process.env.DB_USER || "root",
+      password: process.env.DB_PASSWORD || "",
+      database: process.env.DB_NAME || "railway",
       multipleStatements: true,
     },
     migrations: {
