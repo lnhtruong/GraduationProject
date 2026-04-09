1fc5a7a3d6f9bca0934b3de94d5de5f0a497905d
Lê Trường <truongle.25082004@gmail.com>
2026-04-09 12:01:29 +0700

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,33 +1,12 @@
 module.exports = {
   apps: [
-    {
-      name: "auth_service",
-      cwd: "./backend_services/auth_service",
-      script: "dist/main.js",
-      interpreter: "node",
-      env_file: "/opt/graduation-project/backend_services/auth_service/.env",
-    },
     // {
-    //   name: "user_service",
-    //   cwd: "./backend_services/user_service",
+    //   name: "ai_service",
+    //   cwd: "./backend_services/ai_service",
     //   script: "dist/main.js",
     //   interpreter: "node",
-    //   env_file: "/opt/graduation-project/backend_services/user_service/.env",
+    //   env_file: "/opt/graduation-project/backend_services/ai_service/.env",
     // },
-    //   {
-    //     name: "mail_service",
-    //     cwd: "/opt/graduation-project/backend_services/mail_service",
-    //     script: "src/server.js",
-    //     interpreter: "node",
-    //     env_file: "/opt/graduation-project/backend_services/mail_service/.env"
-    //   },
-    //   {
-    //     name: "payment_service",
-    //     cwd: "/opt/graduation-project/backend_services/payment_service",
-    //     script: "src/server.js",
-    //     interpreter: "node",
-    //     env_file: "/opt/graduation-project/backend_services/payment_service/.env"
-    //   }
     {
       name: "api_gateway",
       cwd: "./backend_services/api_gateway",
@@ -36,11 +15,18 @@ module.exports = {
       env_file: "./backend_services/api_gateway/.env",
     },
     {
-      name: "media_service",
-      cwd: "./backend_services/media_service",
+      name: "auth_service",
+      cwd: "./backend_services/auth_service",
       script: "dist/main.js",
       interpreter: "node",
-      env_file: "./backend_services/media_service/.env",
+      env_file: "/opt/graduation-project/backend_services/auth_service/.env",
+    },
+    {
+      name: "course_service",
+      cwd: "./backend_services/course_service",
+      script: "dist/main.js",
+      interpreter: "node",
+      env_file: "/opt/graduation-project/backend_services/course_service/.env",
     },
     {
       name: "inference_service",
@@ -48,6 +34,34 @@ module.exports = {
       script: "dist/main.js",
       interpreter: "node",
       env_file: "./backend_services/inference_service/.env",
+    },
+    //   {
+    //     name: "mail_service",
+    //     cwd: "/opt/graduation-project/backend_services/mail_service",
+    //     script: "src/server.js",
+    //     interpreter: "node",
+    //     env_file: "/opt/graduation-project/backend_services/mail_service/.env"
+    //   },
+    {
+      name: "media_service",
+      cwd: "./backend_services/media_service",
+      script: "dist/main.js",
+      interpreter: "node",
+      env_file: "./backend_services/media_service/.env",
+    },
+    //   {
+    //     name: "payment_service",
+    //     cwd: "/opt/graduation-project/backend_services/payment_service",
+    //     script: "src/server.js",
+    //     interpreter: "node",
+    //     env_file: "/opt/graduation-project/backend_services/payment_service/.env"
+    //   }
+    {
+      name: "user_service",
+      cwd: "./backend_services/user_service",
+      script: "dist/main.js",
+      interpreter: "node",
+      env_file: "/opt/graduation-project/backend_services/user_service/.env",
     },
   ],
 };
